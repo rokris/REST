232c4ae893228a384b42cbe73f0889139a094d10
rokris <rokris@hotmail.com>
2017-12-06 08:08:59 +0100

    Minor fix

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ var mongoose = require('mongoose');
 mongoose.connect(dbConfig.url, { useMongoClient: true });
 
 mongoose.connection.on('error', function() {
-    console.log('Could not connect to the database. Exiting now...');
+    console.log('Could not connect to the database.');
     //process.exit();
 });
+
 mongoose.connection.once('open', function() {
     console.log("Successfully connected to the database");
-})
+});
 
 // define a simple route
 app.get('/', function(req, res){
